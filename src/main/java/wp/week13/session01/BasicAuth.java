16d6649a10a1403864aa8121237756bf540eccae
Karthikeyan Rajendran <Karthikeyan_Rajendran@epam.com>
2025-03-31 20:47:05 +0200

    New approach for BasicAuth using HasAuthentication interface

--- src/main/java/wp/week13/session01/BasicAuth.java
+++ src/main/java/wp/week13/session01/BasicAuth.java
@@ -3,10 +3,13 @@ package wp.week13.session01;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.UsernameAndPassword;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BasicAuth {
+	
+	static ChromeDriver driver;
 
 	public static void main(String[] args) {
 		
@@ -14,13 +17,18 @@ public class BasicAuth {
 		options.addArguments("--disable-search-engine-choice-screen");
 		options.addArguments("--start-maximized");
 		options.addArguments("--headless");
-		ChromeDriver driver = new ChromeDriver(options);		
-		//https://username:password@the-internet.herokuapp.com/basic_auth
-		driver.get("https://admin:admin@the-internet.herokuapp.com/basic_auth");
+		driver = new ChromeDriver(options);		
+		driver.register(UsernameAndPassword.of("admin", "admin"));		
+		driver.get("https://the-internet.herokuapp.com/basic_auth");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 		System.out.println(driver.findElement(By.tagName("h3")).getText());
 		driver.quit();
 
 	}
+	
+	static String basicAuth() {
+		return "https://username:password@the-internet.herokuapp.com/basic_auth";
+	}
+	
 
 }
